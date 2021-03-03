d390ee5bcaefb5fafdb74bd018f8f7add054caac
Jakub Havelka <HamAndRock@users.noreply.github.com>
2021-03-03 14:52:13 +0100

    Absolute Routes, Trailing Slash, Route Caching (#33)
    * Trailing slash fix
    
    * Trailing slash settings
    
    * Absolute routes, Route caching

--- examples/simple/server/index.js
+++ examples/simple/server/index.js
@@ -1,6 +1,8 @@
 // This file doesn't go through babel or webpack transformation.
 // Make sure the syntax and sources this file requires are compatible with the current node version you are running
 // See https://github.com/zeit/next.js/issues/1245 for discussions on Universal Webpack or universal Babel
+// const getNextI18nRoutesMiddleware = require('../../../index').getNextI18nRoutesMiddleware;
+
 const routes = require('./routes').routes;
 const supportedLangs = require('./routes').supportedLangs;
 const express = require("express");
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -5,6 +5,8 @@ import { getPatternSupportedLangs } from '../utils/lang';
 import { NextFunction } from 'connect';
 import { GetNextI18nRoutesMiddleware } from './types';
 
+let cashedPaths : {[key: string]: {data: any, template: string}} = {};
+
 // Has to have any - next does not expose type for app (The type is called "Server" but is deeply nested and not exposed to the public)
 const getNextI18nRoutesMiddleware: GetNextI18nRoutesMiddleware = (
   server,
@@ -20,18 +22,52 @@ const getNextI18nRoutesMiddleware: GetNextI18nRoutesMiddleware = (
       })
     }
 
-    server.get(`${routePatternSupportedLangs}/*`, (reqEndpoint: Request, resEndpoint: Response) => {
-      const path = getPath(reqEndpoint.url);
+    if (settings.trailingSlashRedirect === undefined) settings.trailingSlashRedirect = true;
+
+    server.get(`${routePatternSupportedLangs}*`, (reqEndpoint: Request, resEndpoint: Response) => {
+
+      const cache = cashedPaths[reqEndpoint.url];
+
+      //Checks cache
+      if (cache) {
+        return app.render(
+            reqEndpoint,
+            resEndpoint,
+            cache.template,
+            cache.data
+        );
+      }
+
+      let path = getPath(reqEndpoint.url);
 
       // Create response query from "lang dynamic parameter" + query string
       const lang = reqEndpoint.params.lang;
       const query = parse(reqEndpoint.url, true).query;
       const responseQuery = {...query, lang};
 
+      // Strip trailing slash from url
+      let trailingSlash = false;
+      if (path.substr(-1) === '/') {
+        path = path.substring(0, path.length-1)
+        trailingSlash = true;
+      }
+
       // Get RouteMatchedObject out of path
       const routeMatchedObject = getRouteMatchedObject(settings.routes, path, lang);
+
+      // Redirects to url without trailing slash.
+      if (settings.trailingSlashRedirect && trailingSlash) {
+        resEndpoint.redirect(301, path);
+        return;
+      }
+
       if (!routeMatchedObject) {
         return app.render(reqEndpoint, resEndpoint, path, responseQuery);
+      }
+
+      cashedPaths[reqEndpoint.url] = {
+        template: routeMatchedObject.template,
+        data: { ...responseQuery, ...routeMatchedObject.params }
       }
 
       // Render matched route + add dynamic params to the query object
@@ -45,9 +81,16 @@ const getNextI18nRoutesMiddleware: GetNextI18nRoutesMiddleware = (
 
     const handle = app.getRequestHandler();
 
-    server.get("*", (req, res) => {
-      handle(req, res);
-    });
+    //prevents from double getProps calling in devEnviroment
+    if (process.env.NODE_ENV !== 'production') {
+      server.get(/^(?:(?!_next\/data).)*$/, (req, res) => {
+        return handle(req, res);
+      });
+    } else {
+      server.get('*', (req, res) => {
+        return handle(req, res);
+      });
+    }
 
     server.post('*', (req, res) => {
       return handle(req, res)
